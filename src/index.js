b6e0052da9699f0f8265f047d44272969fde6879
Alexey Brambalov <brambalov@gmail.com>
2019-10-03 21:45:49 +0100

    js-edu

--- src/index.js
+++ src/index.js
@@ -4,11 +4,14 @@
  * @param config - private student ability to perform for different focus modes
  * @returns number of weeks needed for finish education
  */
-module.exports = function getTimeForEducation(
-    focus = 'family', 
-    knowsProgramming = true,
-    config = {family: 4}
-    ) {
-      return 0;
-  };
-  
+
+module.exports = function getTimeForEducation(focus, knowsProgramming, config) {
+  let totalHours, weeks;
+  if (knowsProgramming) {
+    totalHours = 800;
+  } else {
+    totalHours = 1300;
+  }
+  weeks = Math.ceil(totalHours / config[focus]);
+  return weeks;
+};
